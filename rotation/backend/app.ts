f05cafb0470d0360d13c2afa72b3e12284332f96
Jack Tracy Totten <jacktracytotten09@gmail.com>
2026-02-17 14:22:35 -0500

    scheme v.1

---
+++ rotation/backend/app.ts
@@ -0,0 +1,29 @@
+// index.ts
+// Query your database using the Prisma Client
+
+import 'dotenv/config'
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+// Example query to create a user based on the example schema
+
+async function main() {
+
+
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
