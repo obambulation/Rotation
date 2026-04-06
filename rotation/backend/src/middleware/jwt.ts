238c337b2a153c5f9c79867e87ea6fd9cefb7184
Jack Tracy Totten <jacktracytotten09@gmail.com>
2026-04-06 11:41:38 -0400

    fixed jwt

--- rotation/backend/src/middleware/jwt.ts
+++ rotation/backend/src/middleware/jwt.ts
@@ -1,12 +1,13 @@
 import type { Request, Response, NextFunction } from "express";
 import AppError from "../utils/apperror.js";
+import prisma from "../lib/prisma.js";
 import jwt from "jsonwebtoken";
 
 type AuthTokenPayload = {
     id: string;
 }
 
-export const protect = (req: Request, res: Response, next: NextFunction) => {
+export const protect = async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
     if(!authHeader){
         throw new AppError("Unauthorized (DOES NOT HAVE BEARER DELETE AT PROD)", 401);
@@ -21,7 +22,15 @@ export const protect = (req: Request, res: Response, next: NextFunction) => {
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as AuthTokenPayload;
-        req.user = decoded;
+        const user = await prisma.user.findUnique({
+            where: { id: decoded.id },
+            select: { id: true},
+            
+        });
+        if(!user){
+            throw new AppError("Unauthorized (USER NOT FOUND DELETE AT PROD)", 401);
+        }
+        req.user = user;
         next();
     }
     catch (err) {
