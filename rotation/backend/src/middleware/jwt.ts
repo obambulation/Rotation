f4df0ce0c4903be675bf0dabd75be443663988a0
Jack Tracy Totten <jacktracytotten09@gmail.com>
2026-04-06 11:49:37 -0400

    fixed jwt

--- rotation/backend/src/middleware/jwt.ts
+++ rotation/backend/src/middleware/jwt.ts
@@ -8,32 +8,33 @@ type AuthTokenPayload = {
 }
 
 export const protect = async (req: Request, res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        throw new AppError("Unauthorized (DOES NOT HAVE BEARER DELETE AT PROD)", 401);
 
-    }
-    else if (!authHeader.startsWith("Bearer ")){
-        throw new AppError("Unauthorized (DOES NOT START WITH BEARER DELETE AT PROD)", 401);
-    }
-    const token = authHeader.split(" ")[1];
-    if(!token){
-        throw new AppError("Unauthorized (NO TOKEN DELETE AT PROD)", 401);
-    }
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as AuthTokenPayload;
+        const authHeader = req.headers.authorization;
+        if(!authHeader || !authHeader.startsWith("Bearer ")){
+            return next(new AppError("Unauthorized (DOES NOT HAVE BEARER DELETE AT PROD)", 401));
+
+        }
+        const token = authHeader.split(" ")[1];
+        if(!token){
+            return next(new AppError("Unauthorized (NO TOKEN DELETE AT PROD)", 401));
+        }
+        if(!process.env.JWT_SECRET){
+            return next(new AppError("JWT_SECRET is not defined in environment variables", 500));
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) as AuthTokenPayload;
         const user = await prisma.user.findUnique({
             where: { id: decoded.id },
             select: { id: true},
             
         });
         if(!user){
-            throw new AppError("Unauthorized (USER NOT FOUND DELETE AT PROD)", 401);
+            return next(new AppError("Unauthorized (USER NOT FOUND DELETE AT PROD)", 401));
         }
         req.user = user;
-        next();
+        return next();
     }
     catch (err) {
-        throw new AppError('Unauthorized (WRONG TOKEN DELETE AT PROD) ', 401);
+        return next(new AppError('Unauthorized (WRONG TOKEN DELETE AT PROD) ', 401));
     }
 }
